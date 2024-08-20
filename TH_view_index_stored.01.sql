04dc2687ad1144dd5a0a42266593e883d32eaaec
kỳ.vũ <phucky1235@gmail.com>
2024-08-20 16:45:30 +0700

    TH01-02-03-04

--- TH_view_index_stored.01.sql
+++ TH_view_index_stored.01.sql
@@ -236,4 +236,19 @@ CALL SetCounter(@counter,1);
 CALL SetCounter(@counter,5); 
 SELECT @counter; 
 
+create view customer_views as 
+select customerNumber, customerName, phone
+from customers;
+
+select*from customer_views;
+
+create or replace view customer_views as
+select customerNumber, customerName, contactFirstName, contactLastName, phone
+from customers
+where city = 'Nantes';
+
+select*from customer_views;
+
+drop view customer_views;
+
 
